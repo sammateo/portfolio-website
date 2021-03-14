f5cb772d6ac167c1b630d8a861db7bb93891a9e2
Mateo Sam <sammateo4@gmail.com>
2021-03-13 21:10:39 -0400

    responsiveness

--- src/project.js
+++ src/project.js
@@ -2,12 +2,12 @@ import style from '../styles/Project.module.css'
 import Link from 'next/link'
 export default function project({title,description,refindex}){
     const projinfo=[
-        {title:"Covid-19 Tracker", description:"Tracks covid 19 statistics", ref:"https://covid-19-stat-tracker.netlify.app/"},
-        {title:"Find-A-Verse", description:"Finds Bible verses", ref:"https://findaverse.netlify.app/"}
+         {ref:"https://covid-19-stat-tracker.netlify.app/"},
+         {ref:"https://findaverse.netlify.app/"}
       ]
     return(
-        <Link href={projinfo[refindex].ref}>
-            <a><div className={style.container}>
+        <Link   href={projinfo[refindex].ref}>
+            <a target="_blank"><div className={style.container}>
             
                 <h3>{title}</h3>
                 <p>{description}</p>
