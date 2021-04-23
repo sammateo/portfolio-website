d49f5625433bd0a79b3b7e03cbc236f33289042c
Mateo Sam <sammateo4@gmail.com>
2021-04-23 13:32:50 -0400

    Took advantage of React Components

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import Header from "../src/Header";
-import Landing from "../src/landing";
+import Landing from "../src/Landing";
 import Projects from "../src/Projects";
-import About from "../src/about";
-import Footer from "../src/footer";
+import About from "../src/About";
+import Footer from "../src/Footer";
 
 export default function Home() {
   return (
