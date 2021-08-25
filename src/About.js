5052acb41dfc530e2bae828e72a79ae0f532446b
Mateo Sam <sammateo4@gmail.com>
2021-08-25 10:14:21 -0400

    changed to second year

--- src/About.js
+++ src/About.js
@@ -3,25 +3,26 @@ import Aos from "aos";
 import { useEffect } from "react";
 import "aos/dist/aos.css";
 export default function project() {
-  useEffect(() => {
-    Aos.init({ duration: 2000 });
-  });
+	useEffect(() => {
+		Aos.init({ duration: 2000 });
+	});
 
-  return (
-    <div id="about" className={styles.about} data-aos="fade-up">
-      <h2>About Mateo</h2>
-      <p>
-        First Year Computer Science Student at the University of the West Indies
-      </p>
+	return (
+		<div id="about" className={styles.about} data-aos="fade-up">
+			<h2>About Mateo</h2>
+			<p>
+				Second Year Computer Science Student at the University of the West
+				Indies - Cave Hill Campus
+			</p>
 
-      <ul>
-        <p>Languages and Frameworks:</p>
-        <li>Javascript</li>
-        <li>ReactJS</li>
-        <li>NextJS</li>
-        <li>C++</li>
-        <li>C</li>
-      </ul>
-    </div>
-  );
+			<ul>
+				<p>Languages and Frameworks:</p>
+				<li>Javascript</li>
+				<li>ReactJS</li>
+				<li>NextJS</li>
+				<li>C++</li>
+				<li>C</li>
+			</ul>
+		</div>
+	);
 }
